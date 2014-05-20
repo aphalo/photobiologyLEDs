1d41c762eca03d84cc64b23c0fc25a707d76e085
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-05-20 20:18:39 +0300

    Added class attributes. Made sure that all data is in data.tables and changed names to .dt Only edited the data building script.

--- raw.data/save.data.r
+++ raw.data/save.data.r
@@ -10,43 +10,51 @@ oldwd <- setwd("raw.data/Maya/examples")
 for (led in c("BS436", "CB30", "LED405", "LED740", "UV395", "white", "XSL365", "XSL370", "XSL375", "white")) {
   temp.led.dt <- process_maya_files(paste(led, ".txt", sep=""), paste(led, "dark.txt", sep=""), 
                                                          method="full", decimal=",")
-  setDT(temp.led.dt)
+  setattr(temp.led.dt, "class", c("source.spct", "generic.spct", class(temp.led.dt)))
+#  setDT(temp.led.dt)
   temp.led.dt[ , s.q.irrad := as_quantum_mol(w.length, s.e.irrad)]
   temp.led.dt[ , s.e.irrad.good := NULL]
-  dt.name <- paste(led, "data", sep=".")
+  dt.name <- paste(led, "dt", sep=".")
   assign(dt.name, temp.led.dt)
-  save(list=dt.name, file=paste(oldwd, "/data/", led, ".data.rda", sep=""))
+  save(list=dt.name, file=paste(oldwd, "/data/", led, ".dt.rda", sep=""))
 }
 
 setwd(oldwd)
 
 setwd("raw.data")
 
-Norlux_R.data <- read.table("NHXRGB0905005_RED.txt", col.names=c("w.length", "s.e.irrad"))
-Norlux_G.data <- read.table("NHXRGB0905005_GREEN.txt", col.names=c("w.length", "s.e.irrad"))
-Norlux_B.data <- read.table("NHXRGB0905005_BLUE.txt", col.names=c("w.length", "s.e.irrad"))
-
-save(Norlux_R.data, Norlux_G.data, Norlux_B.data, file=paste(oldwd, "/data/Norlux_RGB.rda", sep=""))
+Norlux_R.dt <- read.table("NHXRGB0905005_RED.txt", col.names=c("w.length", "s.e.irrad"))
+Norlux_G.dt <- read.table("NHXRGB0905005_GREEN.txt", col.names=c("w.length", "s.e.irrad"))
+Norlux_B.dt <- read.table("NHXRGB0905005_BLUE.txt", col.names=c("w.length", "s.e.irrad"))
+class(Norlux_R.dt) <- c("source.spct", "generic.spct", class(Norlux_R.dt))
+class(Norlux_G.dt) <- c("source.spct", "generic.spct", class(Norlux_G.dt))
+class(Norlux_B.dt) <- c("source.spct", "generic.spct", class(Norlux_B.dt))
+setDT(Norlux_R.dt)
+setDT(Norlux_G.dt)
+setDT(Norlux_B.dt)
+save(Norlux_R.dt, Norlux_G.dt, Norlux_B.dt, file=paste(oldwd, "/data/Norlux_RGB.rda", sep=""))
 
 setwd(oldwd)
 
 setwd("raw.data/Maya/Huey_Jann")
 
-HJ_Blue.data <- process_maya_files("BLUE_short.txt", "dark_short.txt", "PC_long.txt", "BLUE_long.txt", "dark_long.txt", decimal=",", method="sun")
-setDT(HJ_Blue.data)
-HJ_Blue.data[ , s.q.irrad := as_quantum_mol(w.length, s.e.irrad)]
-HJ_Blue.data[ , s.e.irrad.good := NULL]
-save(HJ_Blue.data, file=paste(oldwd, "/data/HJ_Blue.data.rda", sep=""))
+HJ_Blue.dt <- process_maya_files("BLUE_short.txt", "dark_short.txt", "PC_long.txt", "BLUE_long.txt", "dark_long.txt", decimal=",", method="sun")
+setattr(HJ_Blue.dt, "class", c("source.spct", "generic.spct", class(HJ_Blue.dt)))
+# setDT(HJ_Blue.dt) # redundant
+HJ_Blue.dt[ , s.q.irrad := as_quantum_mol(w.length, s.e.irrad)]
+HJ_Blue.dt[ , s.e.irrad.good := NULL]
+save(HJ_Blue.dt, file=paste(oldwd, "/data/HJ_Blue.dt.rda", sep=""))
 
 setwd(oldwd)
 
 setwd("raw.data/Maya/Shenzhen_Weili")
 
-SW_UVA365.data <- process_maya_files("UVA_short.txt", "dark_short.txt", "PC_long.txt", "UVA_long.txt", "dark_long.txt", decimal=",", method="sun")
-setDT(SW_UVA365.data)
-SW_UVA365.data[ , s.q.irrad := as_quantum_mol(w.length, s.e.irrad)]
-SW_UVA365.data[ , s.e.irrad.good := NULL]
-save(SW_UVA365.data, file=paste(oldwd, "/data/SW_UVA365.data.rda", sep=""))
+SW_UVA365.dt <- process_maya_files("UVA_short.txt", "dark_short.txt", "PC_long.txt", "UVA_long.txt", "dark_long.txt", decimal=",", method="sun")
+setattr(SW_UVA365.dt, "class", c("source.spct", "generic.spct", class(SW_UVA365.dt)))
+# setDT(SW_UVA365.dt) # redundant
+SW_UVA365.dt[ , s.q.irrad := as_quantum_mol(w.length, s.e.irrad)]
+SW_UVA365.dt[ , s.e.irrad.good := NULL]
+save(SW_UVA365.dt, file=paste(oldwd, "/data/SW_UVA365.dt.rda", sep=""))
 
 setwd(oldwd)
 
